181c566a01f989a4fdb16b44bb11212a5315d799
ashinshanly <ashinkoottala@gmail.com>
2025-04-25 14:58:00 +0530

    Live rooms test4

--- server/server.ts
+++ server/server.ts
@@ -98,6 +98,16 @@ io.on('connection', (socket) => {
     // Send current users to the new user
     socket.emit('user-joined', { users: room.users });
     socket.to(roomId).emit('user-joined', { users: room.users });
+
+    // If there's a user sharing in the room, notify the new user
+    const sharingUser = room.users.find((u: any) => u.isSharing);
+    if (sharingUser) {
+      console.log(`Notifying new user ${socket.id} about sharing user ${sharingUser.id}`);
+      socket.emit('user-started-sharing', {
+        userId: sharingUser.id,
+        username: sharingUser.name
+      });
+    }
   });
 
   // Handle start sharing
