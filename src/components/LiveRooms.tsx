2853719ac909d55f0ee6c0eefe99c27187b5178f
ashinshanly <ashinkoottala@gmail.com>
2025-04-29 20:12:58 +0530

    Changed video icon to microphone icon in room listings

--- src/components/LiveRooms.tsx
+++ src/components/LiveRooms.tsx
@@ -188,7 +188,7 @@ const LiveRooms: React.FC = () => {
               <div className="flex justify-between items-start mb-3">
                 <h3 className="text-xl font-semibold text-white flex items-center">
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 text-purple-400" viewBox="0 0 20 20" fill="currentColor">
-                    <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z" />
+                    <path fillRule="evenodd" d="M7 4a3 3 0 016 0v4a3 3 0 11-6 0V4zm4 10.93A7.001 7.001 0 0017 8a1 1 0 10-2 0A5 5 0 015 8a1 1 0 00-2 0 7.001 7.001 0 006 6.93V17H6a1 1 0 100 2h8a1 1 0 100-2h-3v-2.07z" clipRule="evenodd" />
                   </svg>
                   {room.name}
                 </h3>
